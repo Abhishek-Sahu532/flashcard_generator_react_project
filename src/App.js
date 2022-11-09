8cee917036e7b44ae57c8a8a12fef5a633dd48b4
Abhishek Sahu <asahu532@gmail.com>
2022-11-09 14:54:41 +0530

    commit starter  code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import CreateFlashcard from './Components/CreateFlashcard/CreateFlashcard';
+
+function App() {
+  return (
+    <div className="App">
+      
+    <CreateFlashcard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/CreateFlashcard/CreateFlashcard.js
@@ -0,0 +1,15 @@
+import React from "react"; 
+
+function CreateFlashcard(){
+    return(
+        <>
+        <div className="container mx-auto bg-slate-300">
+        <h1 className="text-lg font-bold">Create Flashcard</h1>
+
+        </div>
+        </>
+
+    )
+}
+
+export default CreateFlashcard;
