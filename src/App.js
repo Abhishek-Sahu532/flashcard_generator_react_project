e26f97c698882a3bf5a959fa3986d8d8803c9fe3
Abhishek Sahu <asahu532@gmail.com>
2022-11-14 18:17:46 +0530

    router created & added form validation using formik

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 
 import './App.css';
-import CreateFlashcard from './Components/CreateFlashcard/CreateFlashcard';
-import MyFlashcard from './Components/MyFlashcard';
+// import CreateFlashcard from './Components/CreateFlashcard/CreateFlashcard';
+// import MyFlashcard from './Components/MyFlashcard';
 import Navbar from './Components/Navbar'
+// import ShowMyFlashcard from './Components/ShowMyFlashcard'
+import {Link} from 'react-router-dom'
+import Router1  from './Router/Router1';  
+
 
 function App() {
   return (
-    <div className="App bg-slate-100 h-screen">
+    <div className="App b g-slate-100 h-screen">
       <Navbar />
-    <CreateFlashcard />
-    <MyFlashcard/>
+<div className='text-lg font-normal ml-10 text-slate-500 pl-8 pr-12'>
+  
+<Link to="/" >Create New</Link>
+<Link className='ml-3' to="showmyflash" >My Flashcard</Link>
+<hr/>
+</div>
+     <Router1 />
     </div>
   );
 }
--- src/Components/CreateFlashcard/FlashcardDetails.js
+++ src/Components/CreateFlashcard/FlashcardDetails.js
@@ -3,20 +3,31 @@ import {useFormik} from 'formik'
 
 
 function FlashcardDetails(){
+
  const formik = useFormik({
+
+    
     initialValues: {
         addTerm: '',
         addDefination:'',
         imageUpload:''
+    },
+    
+    validate: values=>{
+        let error ={}
+
+        if(!values.addTerm){
+            error.addTerm ='Required'
+        }
+        return error
     }
     
  }) 
 
- let inputValue = formik.values
-
+ console.log(formik.values)
 
     return(
-<div className="p-3 bg-slate-300  mx-10 my-10">
+<div className="p-3 bg-slate-300  mx-10 my-10 mx-auto">
     <form onSubmit={formik.onSubmit}>
 <table className='table-auto'>
     <thead className='text-center'>
@@ -30,10 +41,13 @@ function FlashcardDetails(){
     <tbody>
         <tr className='mx-7'>
     <td className='bg-red-400 w-1/2 p-2 text-white font-bold rounded-full'>1</td>
-
+{/* INPUT FOR ADD TERM */}
         <td > <input className='w-80 rounded mx-12 p-2 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-50 rounded-md sm:text-sm focus:ring-1' type="text" name='addTerm' id='addTerm' onChange={formik.handleChange} value={formik.values.addTerm} /> </td>
+        
+{/* INPUT FOR DEFINITION */}
         <td> <input className='w-80 rounded mx-12 p-2 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-50 rounded-md sm:text-sm focus:ring-1' type="text" name='addDefination' id='addDefination' onChange={formik.handleChange} value={formik.values.addDefination} /></td>
         <td>
+            {/* BUTTON TO CHOOSE FILE */}
             <button> <input type="file" name="imageFile" id="imageFile" className=" w-15 mx-6 w-30 file:mr-4 file:py-2 file:px-4 file:p-4 file:border-0 file:text-sm file:font-semibold file:bg-white file:text-violet-700 hover:file:bg-violet-100 text sm text-slate-300" onChange={formik.handleChange} value={formik.values.imageUpload} /></button>
         </td>
 
@@ -41,6 +55,7 @@ function FlashcardDetails(){
 
     </tbody>
 </table>
+{/* MAIN BUTTON TO CREATE CARDS */}
 <button className="bg-transparent  text-blue-700 font-semibold hover:text-white py-2 px-4 border border-none hover:border-transparent rounded mt-5" name='imageUpload' id='imageUpload'>
  +Add More
 </button>
