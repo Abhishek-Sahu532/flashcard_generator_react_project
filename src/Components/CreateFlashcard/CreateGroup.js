502203641d7612aba5625715941b9fd2c87b7d3f
abhishek <asahu532@gmail.com>
2022-11-18 23:01:42 +0530

    added advance formik to second component

--- src/Components/CreateFlashcard/CreateGroup.js
+++ src/Components/CreateFlashcard/CreateGroup.js
@@ -1,24 +1,32 @@
 import React from "react";
-import {useFormik} from 'formik'
+import { useFormik } from "formik";
 function CreateGroup() {
+  const formikCreateGroup = useFormik({
+    initialValues: {
+      createGroup: "",
+      imageFile: "",
+      groupDescription: "",
+    },
+    validate :values=>{
+      let error ={};
+      if(!values.createGroup){
+        error.createGroup ='Required'
+      }
+      return error
 
-const formikCreateGroup = useFormik({
-initialValues:{
-  createGroup:'',
-  imageFile: '',
-  groupDescription:''
-}
-  
-})
+    }
+  });
 
-
-console.log(formikCreateGroup.values)
+  console.log(formikCreateGroup.values);
 
   return (
     <div className="p-5 bg-slate-300 mt-1 mx-10 my-10">
-      <form>
+      <form className="form-control">
         <label
-          className="relative block decoration-gray-300 text-lg font-normal text-slate-700"          htmlFor="createGroup">          {" "}
+          className="relative block decoration-gray-300 text-lg font-normal text-slate-700"
+          htmlFor="createGroup"
+        >
+          {" "}
           Create Group
           <span className="after:content-['*'] form-label inline-block mb-2 "></span>
         </label>
@@ -29,8 +37,15 @@ console.log(formikCreateGroup.values)
             type="text"
             name="createGroup"
             id="createGroup"
-            className="w-64 mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-50 rounded-md sm:text-sm focus:ring-1" onChange={formikCreateGroup.handleChange} value={formikCreateGroup.values.createGroup}
+            className="w-64 mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-50 rounded-md sm:text-sm focus:ring-1"
+            onChange={formikCreateGroup.handleChange}
+            value={formikCreateGroup.values.createGroup}
+            onBlur={formikCreateGroup.handleBlur}
           />
+          {formikCreateGroup.touched.createGroup &&
+          formikCreateGroup.errors.createGroup ? (
+            <div className="erros">{formikCreateGroup.errors.createGroup}</div>
+          ) : null}
           {/* BUTTON TO CHOOSE GROUP AVATAR */}
           <label htmlFor="imageFile"></label>
 
@@ -38,7 +53,9 @@ console.log(formikCreateGroup.values)
             type="file"
             name="imageFile"
             id="imageFile"
-            className="w-30 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-white file:text-violet-700 hover:file:bg-violet-100 text sm text-slate-300" onChange={formikCreateGroup.handleChange} value={formikCreateGroup.values.imageFile}
+            className="w-30 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-white file:text-violet-700 hover:file:bg-violet-100 text sm text-slate-300"
+            onChange={formikCreateGroup.handleChange}
+            value={formikCreateGroup.values.imageFile}
           />
 
           {/* GROUP DESCRIPTION */}
@@ -54,7 +71,9 @@ console.log(formikCreateGroup.values)
           name="groupDescription"
           rows="4"
           className=" block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-50 rounded-md sm:text-sm focus:ring-1 resize-none"
-          placeholder="Describe the roles, responsibility, skills required for the job and help candidate understand the role better."  onChange={formikCreateGroup.handleChange} value={formikCreateGroup.values.groupDescription}
+          placeholder="Describe the roles, responsibility, skills required for the job and help candidate understand the role better."
+          onChange={formikCreateGroup.handleChange}
+          value={formikCreateGroup.values.groupDescription}
         ></textarea>
       </form>
     </div>
--- src/Components/CreateFlashcard/FlashcardDetails.js
+++ src/Components/CreateFlashcard/FlashcardDetails.js
@@ -1,70 +1,126 @@
 import React from 'react'
-import {useFormik} from 'formik'
-
+// import { useFormik} from 'formik'
+import {Formik, Form, Field, ErrorMessage} from 'formik'
+import * as Yup from 'yup'
 
 function FlashcardDetails(){
 
- const formik = useFormik({
+const initialValues = {
+  addTerm: "",
+  addDefination: "",
+  imageUpload: "",
+};
 
-    
-    initialValues: {
-        addTerm: '',
-        addDefination:'',
-        imageUpload:''
-    },
-    
-    validate: values=>{
-        let error ={}
-
-        if(!values.addTerm){
-            error.addTerm ='Required'
-        }
-        return error
-    }
-    
- }) 
-
- console.log(formik.values)
-
-    return(
-<div className="p-3 bg-slate-300  mx-10 my-10 mx-auto">
-    <form onSubmit={formik.onSubmit}>
-<table className='table-auto'>
-    <thead className='text-center'>
-    <tr>
-        <td ></td>
-        <td className='decoration-gray-300 text-lg font-bold text-slate-700'>Enter Term<span className="after:content-['*'] form-label inline-block mb-2 "></span></td>
-        <td className='decoration-gray-300 text-lg font-bold text-slate-700'>Enter Defination<span className="after:content-['*'] form-label inline-block mb-2 "></span></td>
-        <td></td>
-    </tr>
-    </thead>
-    <tbody>
-        <tr className='mx-7'>
-    <td className='bg-red-400 w-1/2 p-2 text-white font-bold rounded-full'>1</td>
-{/* INPUT FOR ADD TERM */}
-        <td > <input className='w-80 rounded mx-12 p-2 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-50 rounded-md sm:text-sm focus:ring-1' type="text" name='addTerm' id='addTerm' onChange={formik.handleChange} value={formik.values.addTerm} /> </td>
-        
-{/* INPUT FOR DEFINITION */}
-        <td> <input className='w-80 rounded mx-12 p-2 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-50 rounded-md sm:text-sm focus:ring-1' type="text" name='addDefination' id='addDefination' onChange={formik.handleChange} value={formik.values.addDefination} /></td>
-        <td>
-            {/* BUTTON TO CHOOSE FILE */}
-            <button> <input type="file" name="imageFile" id="imageFile" className=" w-15 mx-6 w-30 file:mr-4 file:py-2 file:px-4 file:p-4 file:border-0 file:text-sm file:font-semibold file:bg-white file:text-violet-700 hover:file:bg-violet-100 text sm text-slate-300" onChange={formik.handleChange} value={formik.values.imageUpload} /></button>
-        </td>
-
-    </tr>
-
-    </tbody>
-</table>
-{/* MAIN BUTTON TO CREATE CARDS */}
-<button className="bg-transparent  text-blue-700 font-semibold hover:text-white py-2 px-4 border border-none hover:border-transparent rounded mt-5" name='imageUpload' id='imageUpload'>
- +Add More
-</button>
-</form>
-
-</div>
+const validationSchema = Yup.object({
+  addTerm: Yup.string().required('Required'),
+  addDefination: Yup.string().required('Required')
+})
 
 
-    )
+// const validate = values =>{
+//   let error = {};
+//   if (!values.addTerm) {
+//     error.addTerm = "Required";
+//   }
+//   if (!values.addDefination) {
+//     error.addTerm = "Required";
+//   }
+//   return error;
+// }
+
+const onSubmit = values =>{
+  console.log(values)
+}
+//   const formik = useFormik({
+//     initialValues, 
+//     // validate,
+//     validationSchema,
+//     onSubmit
+//  }) 
+
+
+    return (
+      <div className="p-3 bg-slate-300  mx-10 my-10 mx-auto">
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={onSubmit}
+        >
+          <Form>
+            <table className="table-auto">
+              <thead className="text-center">
+                <tr>
+                  <td></td>
+                  <td className="decoration-gray-300 text-lg font-bold text-slate-700">
+                    Enter Term
+                    <span className="after:content-['*'] form-label inline-block mb-2 "></span>
+                  </td>
+                  <td className="decoration-gray-300 text-lg font-bold text-slate-700">
+                    Enter Defination
+                    <span className="after:content-['*'] form-label inline-block mb-2 "></span>
+                  </td>
+                  <td></td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="mx-7">
+                  <td className="bg-red-400 w-1/2 p-2 text-white font-bold rounded-full">
+                    1
+                  </td>
+                  {/* INPUT FOR ADD TERM */}
+                  <td>
+                    <div className="form-control">
+                      <Field
+                        className="w-80 rounded mx-12 p-2 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-50 rounded-md sm:text-sm focus:ring-1"
+                        type="text"
+                        name="addTerm"
+                        id="addTerm"
+                      />
+                      <ErrorMessage name="addTerm" />
+                    </div>
+                  </td>
+
+                  {/* INPUT FOR DEFINITION */}
+                  <td>
+                    {" "}
+                    <div className="form-control">
+                      <Field
+                        className="w-80 rounded mx-12 p-2 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-50 rounded-md sm:text-sm focus:ring-1"
+                        type="text"
+                        name="addDefination"
+                        id="addDefination"
+                      />
+                      <ErrorMessage name="addDefination" />
+                    </div>
+                  </td>
+                  <td>
+                    {/* BUTTON TO CHOOSE FILE */}
+                    <button>
+                      {" "}
+                      <Field
+                        type="file"
+                        name="imageFile"
+                        id="imageFile"
+                        className=" w-15 mx-6 w-30 file:mr-4 file:py-2 file:px-4 file:p-4 file:border-0 file:text-sm file:font-semibold file:bg-white file:text-violet-700 hover:file:bg-violet-100 text sm text-slate-300"
+                      />
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+
+            {/* BUTTON TO ADD ROW */}
+            <button
+              className="bg-transparent  text-blue-700 font-semibold hover:text-white py-2 px-4 border border-none hover:border-transparent rounded mt-5"
+              name="addRow"
+              id="addRow"
+            >
+              +Add More
+            </button>
+          </Form>
+        </Formik>
+      </div>
+    );
 }
 
 
