6d127fe4ce24b737ab9f07f765afd75b28458b9f
Abhishek Sahu <asahu532@gmail.com>
2022-11-25 13:19:40 +0530

    error solved

--- src/Components/CreateFlashcard/CreateFlashcard.js
+++ src/Components/CreateFlashcard/CreateFlashcard.js
@@ -1,18 +1,20 @@
 import React from "react";
 import CreateGroup from "./CreateGroup";
 import FlashcardDetails from "./FlashcardDetails";
-// import { useState } from "react";
+
 import { useDispatch } from "react-redux";
-import {bindActionCreators} from 'redux'
+import { bindActionCreators } from "redux";
 //parent
-// import { add_to_cart } from "../../ReduxState/Action/SetData";
-import actionCreators from '../../ReduxState/index'
+
+
+import { actionCreatores } from "../../ReduxState/Action/index";
+
+
 //callback funtion to get data from fist child
 function CreateFlashcard() {
-
-  let dataFromParent ={}
+  let dataFromParent = {};
   let handleCallback = (childData) => {
-    dataFromParent += childData
+    dataFromParent += childData;
   };
 
   //callback function to get data from second child
@@ -20,19 +22,12 @@ function CreateFlashcard() {
     console.log(childData);
   };
 
-//for state update
+  //for state update
 
-const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-const { add_to_cart } = bindActionCreators(actionCreators, dispatch);
+  const { add_to_cart, delete_to_cart } = bindActionCreators(actionCreatores, dispatch);
 
-  //disable the second component and button
-
-  // const [disabled, setDisabled] = useState(true)
-
-  // const disableChange =()=>{
-  //  console.log('abc')
-  // }
   return (
     <>
       <div className="container mt-10 mx-auto ">
@@ -41,7 +36,12 @@ const { add_to_cart } = bindActionCreators(actionCreators, dispatch);
         </h1>
         <CreateGroup parentCallback={handleCallback} />
         <FlashcardDetails parentCallback2={handleCallback2} />
-        <button className="flex justify-center mx-auto my-auto bg-red-500 hover:bg-blue-500 text-white font-bold py-1 w-40  px-1 p-10 rounded" onClick={()=>{dispatch(actionCreators.add_to_cart(dataFromParent));}}>
+        <button
+          className="flex justify-center mx-auto my-auto bg-red-500 hover:bg-blue-500 text-white font-bold py-1 w-40  px-1 p-10 rounded"
+          onClick={() => {
+            dispatch(actionCreatores.add_to_cart(dataFromParent));
+          }}
+        >
           Create
         </button>
       </div>
--- src/ReduxState/Action/SetData.js
+++ src/ReduxState/Action/SetData.js
@@ -1,12 +1,22 @@
 import { SET_CART_DATA } from "./ActionTypes";
+import { DELETE_CART_DATA } from "./ActionTypes";
 
-
-export const add_to_cart =(data)=>{
-  return(dispatch)=>{
+export const add_to_cart = (data) => {
+  return (dispatch) => {
     dispatch({
       type: SET_CART_DATA,
       info: "add the create flashcart data to redux store",
-      payload: data
+      payload: data,
     });
-  }
-}
+  };
+};
+
+export const delete_to_cart = (data) => {
+  return (dispatch) => {
+    dispatch({
+      type: DELETE_CART_DATA,
+      info: "add the create flashcart data to redux store",
+      payload: data,
+    });
+  };
+};
