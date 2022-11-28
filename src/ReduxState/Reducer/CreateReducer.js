da0b16bbe6afaedfaa0bc93973f8f8c43ab0f061
Abhishek Sahu <asahu532@gmail.com>
2022-11-28 17:55:14 +0530

    linked redux and local storage

--- src/ReduxState/Reducer/CreateReducer.js
+++ src/ReduxState/Reducer/CreateReducer.js
@@ -32,3 +32,26 @@ const CreateReducer = (state = initialState, action) => {
 };
 
 export default CreateReducer;
+
+// convert object to string and store in localStorage
+export const saveToLocalStorage =(initialState) =>{
+  try {
+    const serialisedState = JSON.stringify(initialState);
+    localStorage.setItem("persistantState", serialisedState);
+  } catch (e) {
+    console.warn(e);
+  }
+}
+
+// load string from localStarage and convert into an Object
+// invalid output must be undefined
+export const loadFromLocalStorage =() =>{
+  try {
+    const serialisedState = localStorage.getItem("persistantState");
+    if (serialisedState === null) return undefined;
+    return JSON.parse(serialisedState);
+  } catch (e) {
+    console.warn(e);
+    return undefined;
+  }
+}
--- src/ReduxState/Store/Store.js
+++ src/ReduxState/Store/Store.js
@@ -1,5 +1,12 @@
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import reducers from "../Reducer/index";
+import {saveToLocalStorage ,loadFromLocalStorage } from '../Reducer/CreateReducer'
 
-export const Store = createStore(reducers, {}, applyMiddleware(thunk))
+
+
+
+export const Store = createStore(reducers, loadFromLocalStorage(), applyMiddleware(thunk))
+
+
+Store.subscribe(() => saveToLocalStorage(Store.getState()));
