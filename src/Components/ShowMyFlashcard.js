a80d5764f783b1d44aef00d944805f6c68565c41
Abhishek Sahu <asahu532@gmail.com>
2022-12-02 18:14:23 +0530

    updating second mycard component

--- src/Components/ShowMyFlashcard.js
+++ src/Components/ShowMyFlashcard.js
@@ -9,8 +9,11 @@ const ShowMyFlashcard = () => {
   // );
   // console.log(Store.getState())
 
-  const [show, setShow] = useState([""]);
+  const [show, setShow] = useState("");
   let cardValueArr = [];
+  let cardValueArrindex = [];
+
+  
   useEffect(() => {
     let cardValue = localStorage.getItem("cardValue");
     // console.log('abc', cardValue)
@@ -24,11 +27,13 @@ const ShowMyFlashcard = () => {
     for (let i in cardValue) {
       cardValueArr.push(cardValue[i]);
     }
-    // console.log(cardValueArr);
-console.log(cardValueArr)
-    cardValueArr.map((element)=>{
-console.log(element[0].createGroup)
-    })
+    cardValueArrindex = cardValueArr[0]; //to store localstorage array
+    // console.log(cardValueArr)
+    // console.log(cardValueArrindex)
+
+    cardValueArrindex.forEach((element, index) => {
+      console.log(element.createGroup);
+    });
   });
 
   // console.log(createGroup)
@@ -36,31 +41,24 @@ console.log(element[0].createGroup)
     //  {/* SECTION TO SHOW CREATED ALL CARDS*/}
 
     <div className="mx-10 my-10 grid grid-cols-3 gap-4 place-content-around flex flex-wrap justify-items-center">
-      <div className=" shadow-md bg-white p-5 h-64 w-64 m-4 mx-1.5 my-1.5">
-        {
-        
-     cardValueArr.map((element) => (
-          // code having error
-console.log(element.createGroup)
-          // <div>
-          //   <h1 className="text-center font-bold mx-1.5 my-1.5">
-          //     {element.createGroup}
-          //   </h1>
-          //   <div className="text-center bg-white mx-1.5 my-1.5 h-32 w-48">
-          //     <span>{element.groupDescription}</span>
-          //   </div>
-          //   <div className="flex justify-center">
-          //     <button className="rounded-md text-red-600 border-solid border-2 bg-white border-red-700 mx-2 my-2 h-8 w-40">
-          //       View Cards
-          //     </button>
-          //   </div>
-          // </div>
-        ))}
-
-        
-      </div>
+      {cardValueArrindex.forEach((element) => {
+        <div className=" shadow-md bg-white p-5 h-64 w-64 m-4 mx-1.5 my-1.5">
+          <div>
+            <h1 className="text-center font-bold mx-1.5 my-1.5">
+              {element.createGroup}
+            </h1>
+            <div className="text-center bg-white mx-1.5 my-1.5 h-32 w-48">
+              <span>{element.groupDescription}</span>
+            </div>
+            <div className="flex justify-center">
+              <button className="rounded-md text-red-600 border-solid border-2 bg-white border-red-700 mx-2 my-2 h-8 w-40">
+                View Cards
+              </button>
+            </div>
+          </div>
+        </div>;
+      })}
     </div>
-    // <p>{show}</p>
   );
 };
 
